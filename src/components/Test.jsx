f3d773bbf4a210197183d74a8258275f4758174a
Jattyz <jaturon.tepjuk@gmail.com>
2023-03-24 16:54:29 +1100

    start with Astro !"

---
+++ src/components/Test.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+
+const Test = () => {
+  const [num, setNum] = useState(0);
+  const testNum = () => {
+        setNum(num + 1);
+  }
+
+  return (
+    <div>
+        <button onClick={testNum}>Test</button>
+        <h1>{num}</h1>
+    </div>
+  )
+}
+
+export default Test
