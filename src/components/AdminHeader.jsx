66a7ad50d4520a3ec85be2c59cdb75c22a5c1f42
Jaturon Tepjuk <85094200+JATTYz@users.noreply.github.com>
2023-04-22 02:20:34 +1000

    Adminpage (#9)
    * add Upload;
    
    * add prototype
    
    * add admin page
    
    * add aproval table component
    ;
    ;
    
    * add Sidebar

--- src/components/AdminHeader.jsx
+++ src/components/AdminHeader.jsx
@@ -9,8 +9,8 @@ const AdminHeader = () => {
             <h1 class='text-white flex items-center'>Rover Car Club Of Australia</h1>
             {/* <button class='text-white flex items-center text-sm hover:bg-[#361500] transition delay-150 duration-300 ease-in-out'>Login As Admin</button> */}
         </div>  
-
-            <div className='bg-[#361500] text-lg w-full h-10 flex'>
+        
+            {/* <div className='bg-[#361500] text-lg w-full h-10 flex'>
                 <div className='grid grid-cols-6 w-full justify-items-center content-center text-center text-white'>
                     <div className='w-full h-10 hover:bg-[#1C0A00] transition delay-150 duration-300 ease-in-out cursor-pointer'><a href="/">Home</a></div>
                     <div className='w-full hover:bg-[#1C0A00] transition delay-150 duration-300 ease-in-out cursor-pointer'><a href="/admin/approval">Approval Document</a></div>
@@ -18,7 +18,7 @@ const AdminHeader = () => {
                     <div className='w-full hover:bg-[#1C0A00] transition delay-150 duration-300 ease-in-out cursor-pointer'><a href="/admin/analytic">Analytics</a></div>
                     <div className='w-full hover:bg-[#1C0A00] transition delay-150 duration-300 ease-in-out cursor-pointer'><a href="/admin">Admin Dashboard</a></div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
