91a24e86fcc416f9995cb4c64c43e730d27b724a
Jaturon Tepjuk <85094200+JATTYz@users.noreply.github.com>
2023-04-23 01:44:26 +1000

    improve admin page (#10)

--- src/components/ApprovalTable.jsx
+++ src/components/ApprovalTable.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { db } from '../firebase/firebaseConfig'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, getDocs, getDoc, updateDoc } from 'firebase/firestore'
 
 const ApprovalTable = () => {
 
+    const [id, setId] = useState([]);
     const [type, setType] = useState([]);
     const [title, setTitle] = useState([]);
     const [description, setDescription] = useState([]);
     const [date, setDate] = useState([]);
-   
+    const [isOpen, setIsOpen] = useState(true);   
 
     const [isLoading, setIsLoading] = useState(true);
 
@@ -17,11 +18,11 @@ const ApprovalTable = () => {
     async function getTest(){
     const docRef = await getDocs(collection(db, "raac-collection"))
     docRef.forEach((doc) => {
+        setId((prev) => [...prev, doc.id])
         setType((prev) => [...prev, doc.data().type])
         setTitle((prev) => [...prev, doc.data().title])
         setDescription((prev) => [...prev, doc.data().description])
         setDate((prev) => [...prev, doc.data().date])
-      
     })
     setIsLoading(false)
     }
@@ -29,12 +30,98 @@ const ApprovalTable = () => {
     getTest()
     }, [])
 
+    const approveDoc = async (docId) => {
+      const docRef = doc(db, "raac-collection", docId);
+      await updateDoc(docRef, {isApprove: true}).then(
+        alert("SUCCESSFULLY UPDATED")
+      )  
+    }
+
+    const rejectDoc = async (docId) => {
+      const docRef = doc(db, "raac-collection", docId);
+      await updateDoc(docRef, {isApprove: false}).then(
+        alert("SUCCESSFULLY REJECTED DOCUMENT")
+      )  
+    }
+
+
+    const [popTitleDoc, setPopTitleDoc ] = useState();
+
+    const popUpCard = async (docId) => {
+      setIsOpen(true)
+      const docRef = await getDoc(doc(db, "raac-collection",docId))
+      if(docRef.exists()){
+
+      setPopTitleDoc(docRef.data().title)
+      }else{
+        console.log("CAN'T FIND");
+      }
+      console.log("GOOD");
+    }
+
   return (
     <div>
 {isLoading? (
         <div className="absolute top-1/2 left-1/2  h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"role="status"> </div>
         ): (<>
-        <div className="flex flex-col">
+
+        <div className={`bg-blue-500 shadow-lg shadow-blue-500/50 ${!isOpen && "hidden"} rounded-xl w-full`}>
+          <div className='flex justify-between px-6 pt-6'>
+           <h1 className="mb-2 text-3xl font-bold tracking-tight text-white ">Document Upload Details</h1>
+           {/* <button className='bg-orange-700' >test</button> */}
+           <button 
+           onClick={() => setIsOpen(!isOpen)}
+           className="text-white font-bold bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Close</button>
+          </div>
+         <img src="../../public/approve.png" alt="" width={100}/> 
+
+
+
+<div className="flex my-10 w-full justify-center p-4 rounded-lg shadow  bg-gray-800 border-gray-700">
+  <h1 className='text-white text-center'>{popTitleDoc}</h1>
+   {/* <div className="grid grid-cols-2 justify-items-stretch">
+        <ul role="list" className="divide-gray-700 ">
+            <li className="py-3 sm:py-4 ">
+                <div className="grid grid-cols-2 justify-items-center">
+                    <div className="">
+                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Neil Sims
+                        </p>
+                        <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                            email@windster.com
+                        </p>
+                    </div>
+                    <div className="text-base font-semibold text-gray-900 dark:text-white">
+                        $320234
+                    </div>
+
+                </div>
+            </li>
+            </ul>
+   </div> */}
+</div>
+
+         {/* <div className='text-white'>
+          <h1>FF</h1>
+          <h1>FF</h1>
+          <h1>FF</h1>
+          <h1>FF</h1>
+          <h1>FF</h1>
+          <h1>FF</h1>
+          <h1>FF</h1>
+         </div> */}
+         <div className='flex justify-center'>
+          <div>
+          <button className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Approve</button>
+          </div>
+          <div>
+          <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Reject</button>
+          </div>
+         </div>
+        </div>
+
+        <div className={`flex flex-col ${isOpen && "hidden"}`}>
+        {/* <div className={`flex flex-col`}> */}
         <div className="overflow-x-auto">
         <div className="inline-block min-w-full py-2 sm:px-6 lg:px-6">
         <div className="border-4 rounded-lg border-gray-300">
@@ -61,18 +148,24 @@ const ApprovalTable = () => {
               <td className="whitespace-nowrap px-6 py-4">{description[0]}</td>
               <td className="whitespace-nowrap px-6 py-4">Mark</td>
               <td className="whitespace-nowrap px-6 py-4">
-                <button>
+                <button
+                onClick={() => popUpCard(id[0])} 
+                >
                 <img src="/public/dots.png" alt="" className='h-5' />
                 </button>
               </td>
               <td className="whitespace-nowrap px-6 py-4">
-                <button class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded mr-3">
+                <button 
+                onClick={ () => approveDoc(id[0])}
+                className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded mr-3">
                 Approve
                 </button>
-                <button class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded mr-3">
+                <button 
+                onClick={ () => rejectDoc(id[0])}
+                className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded mr-3">
                 Reject
                 </button>
-                <button class="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded">
+                <button className="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded">
                 Need More Info
                 </button>
               </td>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import ApprovalTable from "./ApprovalTable";
+import DoughnutChart from "./DoughnutChart";
+import PolarChart from "./PolarChart";
+import { Radar } from "react-chartjs-2";
+import RadarChart from "./RadarChart";
+import BubbleChart from "./BubbleChart";
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
   const Menus = [
@@ -15,7 +20,7 @@ const Sidebar = () => {
       <div
         className={` ${
           open ? "w-72" : "w-20 "
-        } bg-yellow-700 min-h-screen p-5 pt-8 relative duration-300`}
+        } bg-amber-800 min-h-screen p-5 pt-8 relative duration-300`}
       >
         <img
           src="../../public/control.png"
@@ -58,26 +63,35 @@ const Sidebar = () => {
         </ul>
       </div>
       <div className="min-h-screen flex-1 p-7  bg-gray-200">
-        <h1 className="text-2xl font-semibold ">Document Approval</h1>
+        <h1 className="text-2xl font-extrabold ">Document Approval</h1>
         <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 justify-items-center my-6">
-            <div class="p-5 bg-white rounded-lg shadow-sm dark:bg-amber-800 w-48">
-                <div class="text-base text-gray-400 dark:text-gray-300">Total Pending</div>
-                <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 dark:text-gray-100">3</div>
+            <div className="p-5 bg-white rounded-lg  dark:bg-amber-800 w-48  shadow-lg shadow-amber-950">
+              <div className="flex">
+                <img src="../../public/pending.png" className="w-14  h-12 mr-4" />
+                <div className="text-xl dark:text-gray-300 font-extrabold">Total Pending</div>
+              </div>
+                <div className="items-center pt-1">
+                    <div className="text-2xl font-bold  text-gray-100 pt-4">3</div>
                 </div>
             </div>
 
-            <div class="p-5 bg-white rounded-lg shadow-sm dark:bg-amber-800 w-48">
-                <div class="text-base text-gray-400 dark:text-gray-300">Trend Type</div>
-                <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 dark:text-gray-100">Annoucement</div>
+            <div className="p-5 bg-white rounded-lg dark:bg-amber-800 w-48 shadow-lg shadow-amber-950">
+              <div className="flex">
+                <img src="../../public/trend.png" className="w-14 mr-4 h-12" />
+                <div className="text-xl  text-gray-300 font-extrabold">Trend Type</div>
+              </div>
+                <div className="flex items-center pt-1">
+                    <div className="text-2xl font-bold  text-gray-100 pt-4">Annoucement</div>
                 </div>
             </div>
 
-            <div class="p-5 bg-white rounded-lg shadow-sm dark:bg-amber-800 w-48">
-                <div class="text-base text-gray-400 dark:text-gray-300">Recent Uploaded</div>
-                <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 dark:text-gray-100">22/04/2023</div>
+            <div className="p-5 bg-white rounded-lg dark:bg-amber-800 w-48 shadow-lg shadow-amber-950">
+              <div className="flex">
+                <img src="../../public/update.png" className="w-14 mr-4 h-12" />
+                <div className="text-xl font-extrabold text-gray-300 ">Recent Uploaded</div>
+              </div>
+                <div className="flex items-center pt-1">
+                    <div className="text-2xl font-bold  text-gray-100 pt-4">22/04/2023</div>
                 </div>
             </div>
         </div>
@@ -86,6 +100,34 @@ const Sidebar = () => {
             <ApprovalTable /> 
             </div>
         </div>
+        <div className="mt-20">
+          <h1 className="text-2xl font-extrabold">Analytics</h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full h-auto mt-10">
+          <div>
+           <DoughnutChart />
+            <h1 className="text-center font-bold text-lg mt-10">Document Type</h1>
+          </div>
+          <div>
+            <RadarChart />
+            <h1 className="text-center font-bold text-lg mt-10">Document Upload Trend</h1>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 w-full h-auto mt-20">
+          <div>
+            <BubbleChart />
+            <h1 className="text-center font-bold text-lg">TRAFFIC</h1>
+          </div>
+        </div>
+        <div className="mt-20">
+          <h1 className="text-2xl font-bold">Manually Upload Document</h1>
+          <div className="mt-3 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          {/* <div className="p-5 bg-white rounded-lg  dark:bg-amber-800 w-48  shadow-lg shadow-amber-950"> */}
+              <div className="flex justify-center">
+                <h1 className="text-xl dark:text-gray-300 font-extrabold">Click to upload</h1>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   );
