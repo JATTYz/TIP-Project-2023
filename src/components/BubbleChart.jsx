bf3b791eb0c74264d61b3fddd246c27af28fee0a
Jattyz <jaturon.tepjuk@gmail.com>
2023-04-27 00:14:01 +1000

    Revert "Revert "add data""
    This reverts commit 85b104bcfe96354a38524ebc58dfb6d076cc339d.

--- src/components/BubbleChart.jsx
+++ src/components/BubbleChart.jsx
@@ -23,8 +23,8 @@ export const data = {
     {
       label: 'Red dataset',
       data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
+        x: faker.datatype.number({ min: 0, max: 100000 }),
+        y: faker.datatype.number({ min: 0, max: 100000 }),
         r: faker.datatype.number({ min: 5, max: 20 }),
       })),
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
@@ -32,8 +32,8 @@ export const data = {
     {
       label: 'Blue dataset',
       data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
+        x: faker.datatype.number({ min: 0, max: 100000 }),
+        y: faker.datatype.number({ min: 0, max: 100000 }),
         r: faker.datatype.number({ min: 5, max: 20 }),
       })),
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
