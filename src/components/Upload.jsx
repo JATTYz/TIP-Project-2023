54230785589877d6cdbe8b75b1ba546df9303045
Setareakb <103179600+Setareakb@users.noreply.github.com>
2023-05-17 01:42:28 +1000

    Add files via upload
    Make the upload page aesthetically the same as other parts of the website.

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,52 +1,54 @@
 import React from 'react'
-
+import NewsPaper from '../../public/newspaper.png'
+const boxShadow = '0px 0px 4px 0px #b1b1b1'
 const Upload = () => {
 
   return (
     <div className='h-screen'>
            <h1 className='text-center font-bold text-2xl my-10'>Please Select Upload Documentation Type</h1>
-           <div className='w-full grid md:grid-cols-2 '>
-            <div className='my-3 h-24 text-center ' >
+           <div className='w-full flex justify-around flex-wrap	 '>
+            <div className='my-3 h-24 text-center mr-4 ' >
                 <a href="/upload/AdvertisementNewspaper" >
-                 <button className="text-white border-4 border-amber-950 rounded-lg  bg-amber-800 h-full w-1/2">AdvertisementNewspaper</button>
+                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">Advertisement Newspaper</span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden' src='../../public/newspaper.png' /></div> </button>
                 </a>
             </div>
-            <div  className='my-3 h-24 text-center '>
-                <a href="/upload/ArticleJournal" >
-                 <button className="text-white border-4 border-amber-950 rounded-lg  bg-amber-800 h-full w-1/2">ArticleJournal</button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center '>
-                <a href="/upload/AdvertisementJournal" >
-                 <button className="text-white border-4 border-amber-950 rounded-lg  bg-amber-800 h-full w-1/2">AdvertisementJournal</button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center '>
-                <a href="/upload/ArticleNewspaper" >
-                 <button className="text-white border-4 border-amber-950 rounded-lg  bg-amber-800 h-full w-1/2">ArticleNewspaper</button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center '>
-                <a href="/upload/SaleBrochure" >
-                 <button className="text-white border-4 border-amber-950 rounded-lg  bg-amber-800 h-full w-1/2">SalesBrochure</button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center '>
-                <a href="/upload/SaleRecord" >
-                 <button className="text-white border-4 border-amber-950 rounded-lg  bg-amber-800 h-full w-1/2">SalesRecord</button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center '>
+            <div  className='my-3 h-24 text-center mr-4 '>
                 <a href="/upload/PhotographCommercial" >
-                 <button className="text-white border-4 border-amber-950 rounded-lg  bg-amber-800 h-full w-1/2">PhotographCommercial</button>
+                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Photograph Commercial </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/camera.png' /></div> </button>
                  
                 </a>
             </div>
+            
+            <div  className='my-3 h-24 text-center mr-4 '>
+                <a href="/upload/AdvertisementJournal" >
+                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Advertisement Journal </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/agendas.png' /></div> </button>
+                
+                </a>
+            </div>
+            <div  className='my-3 h-24 text-center mr-4 '>
+                <a href="/upload/ArticleNewspaper" >
+                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Article Newspaper </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/application.png' /></div> </button>
+                
+                </a>
+            </div>
+            <div  className='my-3 h-24 text-center mr-4 '>
+                <a href="/upload/SaleBrochure" >
+                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Sales Brochure </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/sales.png' /></div> </button>
+                
+                </a>
+            </div>
+            <div  className='my-3 h-24 text-center mr-4 '>
+                <a href="/upload/SaleRecord" >
+                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Sales Record </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/acquisition.png' /></div> </button>
+                
+                </a>
+            </div>
+            <div  className='my-3 h-24 text-center mr-4 '>
+                <a href="/upload/ArticleJournal" >
+                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Article Journal </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/copywriting.png' /></div> </button>
+                </a>
+            </div>
+           
            </div> 
     </div>
   )
