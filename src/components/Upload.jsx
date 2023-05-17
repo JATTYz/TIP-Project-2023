c6645821c6172296947572ca26f623edbe5d6f16
sangvo235 <sangvo235@gmail.com>
2023-05-18 00:29:22 +1000

    upload - finalised

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,55 +1,107 @@
 import React from 'react'
-import NewsPaper from '../../public/newspaper.png'
-const boxShadow = '0px 0px 4px 0px #b1b1b1'
+import { RiAdvertisementFill } from 'react-icons/ri'
+import { IoIosJournal } from 'react-icons/io'
+import { FaNewspaper } from 'react-icons/fa'
+import { ImNewspaper } from 'react-icons/im'
+import { HiPhotograph } from 'react-icons/hi'
+import { FaMap } from 'react-icons/fa'
+import { FaRecordVinyl } from 'react-icons/fa'
+import { Card } from '@mantine/core'
+
+
 const Upload = () => {
 
   return (
-    <div className='h-screen'>
-           <h1 className='text-center font-bold text-2xl my-10'>Please Select Upload Documentation Type</h1>
-           <div className='w-full flex justify-around flex-wrap	 '>
-            <div className='my-3 h-24 text-center mr-4 ' >
-                <a href="/upload/AdvertisementNewspaper" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">Advertisement Newspaper</span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden' src='../../public/newspaper.png' /></div> </button>
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/PhotographCommercial" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Photograph Commercial </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/camera.png' /></div> </button>
-                 
-                </a>
-            </div>
-            
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/AdvertisementJournal" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Advertisement Journal </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/agendas.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/ArticleNewspaper" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Article Newspaper </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/application.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/SaleBrochure" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Sales Brochure </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/sales.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/SaleRecord" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Sales Record </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/acquisition.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/ArticleJournal" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Article Journal </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/copywriting.png' /></div> </button>
-                </a>
-            </div>
-           
-           </div> 
+    <div className='grid grid-cols-2'>
+        <div className='col-span-2' >
+            <div class='text-3xl text-center font-bold my-10'>Please Select Upload Documentation Type</div> 
+        </div>
+
+        <div className='col-span-2 flex justify-center gap-x-4 mb-6'>
+            <a href="/upload/AdvertisementJournal">
+                <button className='flex items-end justify-between w-72 px-4 py-4 text-white bg-[#361500] rounded-lg hover:bg-[#1C0A00]'>
+                    <span className='flex items-start mb-10'>
+                        Advertisement Journal
+                    </span>
+                    <span className='flex items-end'>
+                        <RiAdvertisementFill className='mr-2 text-5xl' /> 
+                    </span>
+                </button>
+            </a>
+
+            <a href="/upload/AdvertisementNewspaper">
+                <button className='flex items-end justify-between w-72 px-4 py-4 text-white bg-[#361500] rounded-lg hover:bg-[#1C0A00]'>
+                    <span className='flex items-start mb-10'>
+                        Advertisement Newspaper
+                    </span>
+                    <span className='flex items-end'>
+                        <FaNewspaper className='mr-2 text-5xl' /> 
+                    </span>
+                </button>
+            </a>
+        </div>
+
+        <div className='col-span-2 flex justify-center gap-x-4 mb-6'>
+            <a href="/upload/ArticleJournal">
+                <button className='flex items-end justify-between w-72 px-4 py-4 text-white bg-[#361500] rounded-lg hover:bg-[#1C0A00]'>
+                    <span className='flex items-start mb-10'>
+                        Article Journal
+                    </span>
+                    <span className='flex items-end'>
+                        <IoIosJournal className='mr-2 text-5xl' /> 
+                    </span>
+                </button>
+            </a>
+
+            <a href="/upload/ArticleNewspaper">
+                <button className='flex items-end justify-between w-72 px-4 py-4 text-white bg-[#361500] rounded-lg hover:bg-[#1C0A00]'>
+                    <span className='flex items-start mb-10'>
+                        Article Newspaper
+                    </span>
+                    <span className='flex items-end'>
+                        <ImNewspaper className='mr-2 text-5xl' /> 
+                    </span>
+                </button>
+            </a>
+        </div>
+
+        <div className='col-span-2 flex justify-center gap-x-4 mb-6'>
+            <a href="/upload/PhotographCommercial">
+                <button className='flex items-end justify-between w-72 px-4 py-4 text-white bg-[#361500] rounded-lg hover:bg-[#1C0A00]'>
+                    <span className='flex items-start mb-10'>
+                        Photograph Commercial
+                    </span>
+                    <span className='flex items-end'>
+                        <HiPhotograph className='mr-2 text-5xl' /> 
+                    </span>
+                </button>
+            </a>
+
+            <a href="/upload/SaleBrochure">
+                <button className='flex items-end justify-between w-72 px-4 py-4 text-white bg-[#361500] rounded-lg hover:bg-[#1C0A00]'>
+                    <span className='flex items-start mb-10'>
+                        Sale Brochure
+                    </span>
+                    <span className='flex items-end'>
+                        <FaMap className='ml-2 text-5xl' /> 
+                    </span>
+                </button>
+            </a>
+        </div>
+
+
+        <div className='col-span-2 flex justify-center gap-x-4 mb-6'>
+            <a href="/upload/SaleRecord">
+                <button className='flex items-end justify-between w-72 px-4 py-4 text-white bg-[#361500] rounded-lg hover:bg-[#1C0A00]'>
+                    <span className='flex items-start mb-10'>
+                        Sale Record
+                    </span>
+                    <span className='flex items-end'>
+                        <FaRecordVinyl className='mr-2 text-5xl' /> 
+                    </span>
+                </button>
+            </a>
+        </div>
     </div>
   )
 }
