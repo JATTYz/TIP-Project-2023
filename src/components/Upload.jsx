2890038babfeb3b1cc8818b2b8f1da7a6d03e940
Setareakb <103179600+Setareakb@users.noreply.github.com>
2023-05-18 16:41:01 +1000

    Add files via upload
    layout fixed. size of the buttons fixed. border of icons when it hovers fixed.

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,57 +1,83 @@
-import React from 'react'
-import NewsPaper from '../../public/newspaper.png'
-const boxShadow = '0px 0px 4px 0px #b1b1b1'
-const Upload = () => {
+import React from 'react';
+import NewsPaper from '../../public/newspaper.png';
 
+const Upload = () => {
   return (
-    <div className='h-screen'>
-           <h1 className='text-center font-bold text-2xl my-10'>Please Select Upload Documentation Type</h1>
-           <div className='w-full flex justify-around flex-wrap	 '>
-            <div className='my-3 h-24 text-center mr-4 ' >
-                <a href="/upload/AdvertisementNewspaper" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">Advertisement Newspaper</span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden' src='../../public/newspaper.png' /></div> </button>
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/PhotographCommercial" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Photograph Commercial </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/camera.png' /></div> </button>
-                 
-                </a>
-            </div>
-            
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/AdvertisementJournal" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Advertisement Journal </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/agendas.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/ArticleNewspaper" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Article Newspaper </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/application.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/SaleBrochure" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Sales Brochure </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/sales.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/SaleRecord" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Sales Record </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/acquisition.png' /></div> </button>
-                
-                </a>
-            </div>
-            <div  className='my-3 h-24 text-center mr-4 '>
-                <a href="/upload/ArticleJournal" >
-                 <button class="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden"> <span class="px-4">  Article Journal </span> <div class=" rounded-lg overflow-hidden w-16  flex justify-center" style={{boxShadow}}><img class='w-8 my-1 rounded-lg overflow-hidden ' src='../../public/copywriting.png' /></div> </button>
-                </a>
-            </div>
-           
-           </div> 
+    <div className='h-screen flex justify-center items-start'>
+      <div className='grid grid-cols-2 gap-2 mt-10'>
+        <div className='my-3 text-center' style={{ marginBottom: '-0.90rem' }}>
+          <a href="/upload/AdvertisementNewspaper">
+            <button className="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden w-full">
+              <span className="px-4">Advertisement Newspaper</span>
+              <div className="rounded-lg overflow-hidden w-16 flex justify-center">
+                <img className='w-8 my-1 rounded-lg overflow-hidden' src={NewsPaper} alt='Advertisement Newspaper' />
+              </div>
+            </button>
+          </a>
+        </div>
+        <div className='my-3 text-center' style={{ marginBottom: '-0.90rem' }}>
+          <a href="/upload/PhotographCommercial">
+            <button className="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden w-full">
+              <span className="px-4">Photograph Commercial</span>
+              <div className="rounded-lg overflow-hidden w-16 flex justify-center">
+                <img className='w-8 my-1 rounded-lg overflow-hidden' src='../../public/camera.png' alt='Photograph Commercial' />
+              </div>
+            </button>
+          </a>
+        </div>
+        <div className='my-3 text-center' style={{ marginBottom: '-0.90rem' }}>
+          <a href="/upload/AdvertisementJournal">
+            <button className="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden w-full">
+              <span className="px-4">Advertisement Journal</span>
+              <div className="rounded-lg overflow-hidden w-16 flex justify-center">
+                <img className='w-8 my-1 rounded-lg overflow-hidden' src='../../public/agendas.png' alt='Advertisement Journal' />
+              </div>
+            </button>
+          </a>
+        </div>
+        <div className='my-3 text-center' style={{ marginBottom: '-0.90rem' }}>
+          <a href="/upload/ArticleNewspaper">
+            <button className="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden w-full">
+              <span className="px-4">Article Newspaper</span>
+              <div className="rounded-lg overflow-hidden w-16 flex justify-center">
+                <img className='w-8 my-1 rounded-lg overflow-hidden' src='../../public/application.png' alt='Article Newspaper' />
+              </div>
+            </button>
+          </a>
+        </div>
+        <div className='my-3 text-center' style={{ marginBottom: '-0.90rem' }}>
+          <a href="/upload/SaleBrochure">
+            <button className="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden w-full">
+              <span className="px-4">Sales Brochure</span>
+              <div className="rounded-lg overflow-hidden w-16 flex justify-center">
+                <img className='w-8 my-1 rounded-lg overflow-hidden' src='../../public/sales.png' alt='Sales Brochure' />
+              </div>
+            </button>
+          </a>
+        </div>
+        <div className='my-3 text-center' style={{ marginBottom: '-0.90rem' }}>
+          <a href="/upload/SaleRecord">
+            <button className="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden w-full">
+              <span className="px-4">Sales Record</span>
+              <div className="rounded-lg overflow-hidden w-16 flex justify-center">
+                <img className='w-8 my-1 rounded-lg overflow-hidden' src='../../public/acquisition.png' alt='Sales Record' />
+              </div>
+            </button>
+          </a>
+        </div>
+        <div className='my-3 text-center' style={{ marginBottom: '-0.90rem' }}>
+          <a href="/upload/ArticleJournal">
+            <button className="mt-4 text-white bg-[#361500] rounded-lg hover:bg-[#5f2703] flex justify-between items-center overflow-hidden w-full">
+              <span className="px-4">Article Journal</span>
+              <div className="rounded-lg overflow-hidden w-16 flex justify-center">
+                <img className='w-8 my-1 rounded-lg overflow-hidden' src='../../public/copywriting.png' alt='Article Journal' />
+              </div>
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Upload
+export default Upload;
